8fd479f86b2c549ff6c3515427f8d2a819f0ced4
Fathia Mohammed <mohammedfo5429@gmail.com>
2018-12-09 21:45:00 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,7 +11,16 @@ var cards = [ "queen", "queen", "king", "king" ]
 
 var cardsInPlay = []
 
-var cardOne = cards [0]
+var checkForMatch = function() { if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+} 
+}
+
+var flipCard = function (cardId) {
+/*
+	var cardOne = cards [0]
 cardsInPlay.push('cardOne');
 
 console.log("User flipped " + cardOne);
@@ -19,11 +28,23 @@ console.log("User flipped " + cardOne);
 var cardTwo = cards [1]
 cardsInPlay.push('cardTwo')
 console.log("User flipped " + cardTwo)
-
-
-
-if (cardsInPlay.length === 2) {
+*/
+checkForMatch();
+	if (cardsInPlay.length === 2) {
 alert("Sorry, try again.");
 } else if (cardsInPlay[0] === cardsInPlay[1]) {
 alert("You found a match!");
 }
+
+console.log("User flipped " + cards[cardId])
+
+cardsInPlay.push('cards[cardId}')
+
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
