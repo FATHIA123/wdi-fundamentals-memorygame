106c1940c4229b8b751bc9543a30510d66797dec
Fathia Mohammed <mohammedfo5429@gmail.com>
2018-12-09 20:06:07 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,12 +11,12 @@ var cards = [ "queen", "queen", "king", "king" ]
 
 var cardsInPlay = []
 
-var cardOne = cards [3]
+var cardOne = cards [0]
 cardsInPlay.push('cardOne');
 
 console.log("User flipped " + cardOne);
 
-var cardTwo = cards [3]
+var cardTwo = cards [1]
 cardsInPlay.push('cardTwo')
 console.log("User flipped " + cardTwo)
 
